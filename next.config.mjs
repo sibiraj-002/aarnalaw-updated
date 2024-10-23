6fd38db684e6087fe422e56dfc2609de7efb1e16
jalal1985khan <jalal1985khan@gmail.com>
2024-10-24 00:11:46 +0530

    added image remote settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["docs.aarnalaw.com", "aarnalaw.com", "www.aarnalaw.com"], // Add your domain here
+    remotePatterns: ["docs.aarnalaw.com", "aarnalaw.com", "www.aarnalaw.com"], // Add your domain here
   },
 };
 
