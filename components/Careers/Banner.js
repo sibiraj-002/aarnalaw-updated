16f89c4c73670385116f3078fe0f6e0d9809eedb
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2024-11-04 11:41:31 +0530

    Banner images changed in all page

--- components/Careers/Banner.js
+++ components/Careers/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div className="relative h-[60vh] bg-[url('/PracticeArea/PracticeAreas.png')]">
+    <div className="relative h-[60vh] md:bg-[url('/Careers/CareersBanner.jpg')] bg-[url('/Careers/CareersMobileBanner.jpg')] bg-cover bg-center">
       <div className="absolute bottom-0 flex h-[50vh] w-full items-center justify-center">
         <h1 className="text-5xl font-bold text-white">Careers</h1>
       </div>
--- components/Podcasts/Banner.js
+++ components/Podcasts/Banner.js
@@ -17,7 +17,7 @@ export default function Banner({ title }) {
   };
 
   return (
-    <div className="relative h-[60vh] bg-[url('/PracticeArea/PracticeAreas.png')] bg-cover bg-center">
+    <div className="relative h-[60vh] md:bg-[url('/insights/InsightsBanner.jpg')] bg-[url('/insights/InsightsMobileBanner.jpg')] bg-cover bg-center">
       <div className="absolute bottom-0 flex h-[50vh] w-full items-center justify-center bg-black/50">
         <h1 className="text-5xl font-bold tracking-wide text-white">
           {getTitle()}
