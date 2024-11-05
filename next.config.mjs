b25931c7886ac540f272dd719a0b2f6ef3f2fe28
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2024-11-05 15:08:07 +0530

    insights page redirection updated

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:slug',
+        destination: '/insights/:slug',
+        permanent: true, // Set to true for a 308 permanent redirect; false for a 307 temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
