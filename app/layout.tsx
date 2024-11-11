b4f2f704bdfeecb9078cc37e9e44c545517b367f
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2024-11-11 15:19:09 +0530

    Bing Webmaster added in head and robot .js

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
         <ThemeModeScript />
+        <meta name="msvalidate.01" content="A827D56A91561DA21E2E94273F4D52D5" />
       </head>
 
       <body>
@@ -39,19 +40,29 @@ export default function RootLayout({
         {children}
         <Footer />
         <ScrollToTop />
+
+        {/* GTM Noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WJW9WNHQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {/* Tracking Scripts */}
+        <Script src="/tracking.js" strategy="afterInteractive" />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YWL8EWE9MB"
+          strategy="afterInteractive"
+          async
+        />
       </body>
-      <Script src="/tracking.js" />
-      <Script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></Script>
-      <Script id="gtm-ns" strategy="afterInteractive">
-        {`
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJW9WNHQ"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        `}
-      </Script>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-YWL8EWE9MB"
-      ></Script>
     </html>
   );
 }
