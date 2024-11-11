8b798b3aac8461a9f2cf4bf3e619cd01e327d2d8
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2024-11-11 17:07:43 +0530

    URLs redirection updated in insights page

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,18 @@ const nextConfig = {
     return [
       // Insights
       {
+        source: "/art-as-collateral-a-growing-trend-in-secured-lending",
+        destination:
+          "/insights/art-as-collateral-a-growing-trend-in-secured-lending",
+        permanent: true,
+      },
+      {
+        source: "/regulatory-intervention-in-the-indian-financial-sector-analyzing-the-rbis-actions-against-the-lending-practices-of-a-few-nbfcs",
+        destination:
+          "/insights/regulatory-intervention-in-the-indian-financial-sector-analyzing-the-rbis-actions-against-the-lending-practices-of-a-few-nbfcs",
+        permanent: true,
+      },
+      {
         source: "/government-alerts-ip-applicants-about-fraudulent-activities",
         destination:
           "/insights/government-alerts-ip-applicants-about-fraudulent-activities",
@@ -77,6 +89,122 @@ const nextConfig = {
         source: "/fintech-revolutionizing-lives-amid-regulatory-uncertainty",
         destination:
           "/insights/fintech-revolutionizing-lives-amid-regulatory-uncertainty",
+        permanent: true,
+      },
+
+      //
+      {
+        source: "/the-role-of-an-executive-director",
+        destination:
+          "/insights/the-role-of-an-executive-director",
+        permanent: true,
+      },
+      {
+        source: "/supreme-courts-clarifies-the-meaning-of-a-financial-debt-in-insolvency-proceedings",
+        destination:
+          "/insights/supreme-courts-clarifies-the-meaning-of-a-financial-debt-in-insolvency-proceedings",
+        permanent: true,
+      },
+      {
+        source: "/how-final-is-final-a-recent-decision-by-indias-supreme-court-has-set-out-the-special-conditions-in-which-an-arbitral-award-can-be-reviewed-because-of-a-miscarriage-of-justice`",
+        destination:
+          "/insights/how-final-is-final-a-recent-decision-by-indias-supreme-court-has-set-out-the-special-conditions-in-which-an-arbitral-award-can-be-reviewed-because-of-a-miscarriage-of-justice`",
+        permanent: true,
+      },
+      {
+        source: "/when-can-a-party-claim-confidentiality-for-an-arbitration",
+        destination:
+          "/insights/when-can-a-party-claim-confidentiality-for-an-arbitration",
+        permanent: true,
+      },
+      {
+        source: "/new-act-seeks-to-prevent-the-misuse-of-data-in-india",
+        destination:
+          "/insights/new-act-seeks-to-prevent-the-misuse-of-data-in-india",
+        permanent: true,
+      },
+      {
+        source: "/case-update-itc-limited-virginia-house-ors-v-britannia-industries-ltd",
+        destination:
+          "/insights/case-update-itc-limited-virginia-house-ors-v-britannia-industries-ltd",
+        permanent: true,
+      },
+      {
+        source: "/women-in-leadership-and-ip-ms-stuti-agarwal-of-ontum-education",
+        destination:
+          "/insights/women-in-leadership-and-ip-ms-stuti-agarwal-of-ontum-education",
+        permanent: true,
+      },
+      {
+        source: "/intellectual-property-in-cricket",
+        destination:
+          "/insights/intellectual-property-in-cricket",
+        permanent: true,
+      },
+      {
+        source: "/bharat-road-carrier-private-limited-v-rasna-private-limited",
+        destination:
+          "/insights/bharat-road-carrier-private-limited-v-rasna-private-limited",
+        permanent: true,
+      },
+      {
+        source: "/new-regulations-offer-opportunity-for-fast-track-and-emergency-arbitrations",
+        destination:
+          "/insights/new-regulations-offer-opportunity-for-fast-track-and-emergency-arbitrations",
+        permanent: true,
+      },
+      {
+        source: "/fintech-revolutionizing-lives-amid-regulatory-uncertainty",
+        destination:
+          "/insights/fintech-revolutionizing-lives-amid-regulatory-uncertainty",
+        permanent: true,
+      },
+      {
+        source: "/john-maxwell-morgan-v-mcmillan-investment-holdings-pty-ltd",
+        destination:
+          "/insights/john-maxwell-morgan-v-mcmillan-investment-holdings-pty-ltd",
+        permanent: true,
+      },
+      {
+        source: "/parekh-plastichem-distributors-llp-vs-simplex-infrastructure-limited",
+        destination:
+          "/insights/parekh-plastichem-distributors-llp-vs-simplex-infrastructure-limited",
+        permanent: true,
+      },
+      {
+        source: "/case-update-anupam-mittal-vs-people-interactive-india-pvt-ltd-and-others",
+        destination:
+          "/insights/case-update-anupam-mittal-vs-people-interactive-india-pvt-ltd-and-others",
+        permanent: true,
+      },
+      {
+        source: "/how-artificial-intelligence-can-assist-insolvency-practitioners",
+        destination:
+          "/insights/how-artificial-intelligence-can-assist-insolvency-practitioners",
+        permanent: true,
+      },
+      {
+        source: "/why-traditional-knowledge-needs-better-ip-protection",
+        destination:
+          "/insights/why-traditional-knowledge-needs-better-ip-protection",
+        permanent: true,
+      },
+      {
+        source: "/treatment-of-non-compete-non-solicitation-covenants-in-the-eyes-of-the-courts-in-singapore-and-india",
+        destination:
+          "/insights/treatment-of-non-compete-non-solicitation-covenants-in-the-eyes-of-the-courts-in-singapore-and-india",
+        permanent: true,
+      },
+      {
+        source: "/recent-developments-in-indian-insolvency-law",
+        destination:
+          "/insights/recent-developments-in-indian-insolvency-law",
+        permanent: true,
+      },
+      {
+        source: "/indian-parliament-approves-new-mediation-bill",
+        destination:
+          "/insights/indian-parliament-approves-new-mediation-bill",
         permanent: true,
       },
 
