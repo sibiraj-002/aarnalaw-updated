555cfd05dd9400c88ae5150edad4294a3853fce9
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2024-11-12 10:54:15 +0530

    BingsiteAuth.xml created

--- app/robot.js
+++ app/robot.js
@@ -6,6 +6,6 @@ export default function robots() {
       //   disallow: "/private/",
     },
     sitemap: "https://www.aarnalaw.com/sitemap.xml",
-    verificationCode: "A827D56A91561DA21E2E94273F4D52D5",
+    // verificationCode: "A827D56A91561DA21E2E94273F4D52D5",
   };
 }
