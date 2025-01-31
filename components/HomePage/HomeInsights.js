44d22fac07d6e0bc008518c753623ce8abaf9eed
Sibiraj Kandhasamy <sibirajkandhasamy@IVMBM2SibiRaj23.localdomain>
2025-01-31 16:14:32 +0530

    test home insights

--- components/HomePage/HomeInsights.js
+++ components/HomePage/HomeInsights.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { leftArrow, rightArrow } from "../../utils/icons";
 import InsightSlider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -12,66 +12,64 @@ export default function HomeInsights() {
   const [insightsData, setInsightsData] = useState([]);
   const [loading, setLoading] = useState(true);
   const sliderRef = useRef(null);
-  const [page, setPage] = useState(6);
-  const [hasMore, setHasMore] = useState(true);
-  const domain = typeof window !== "undefined" ? window.location.hostname : "";
 
-  const fetchMedia = async (mediaId) => {
-    const mediaResponse = await fetch(
-      `https://docs.aarnalaw.com/wp-json/wp/v2/media/${mediaId}`
-    );
-    const mediaData = await mediaResponse.json();
-    return mediaData.source_url;
-  };
-
-  const fetchContent = useCallback(async () => {
-    setLoading(true);
-    try {
+  useEffect(() => {
+    const fetchInsights = async () => {
+      const domain = window.location.hostname;
       let server;
-      if (domain === `${configData.LIVE_SITE_URL}`) {
+
+      // Determine the correct server based on the domain
+      if (
+        domain === `${configData.LIVE_SITE_URL}` ||
+        domain === `${configData.LIVE_SITE_URL_WWW}`
+      ) {
         server = `${configData.LIVE_PRODUCTION_SERVER_ID}`;
       } else {
         server = `${configData.STAG_PRODUCTION_SERVER_ID}`;
       }
 
-      const [insightsResponse] = await Promise.all([
-        fetch(
-          `${configData.SERVER_URL}posts?_embed&categories[]=13&status[]=publish&production_mode[]=${server}&per_page=${page}`
-        ),
-      ]);
+      try {
+        const page = 8; // You can modify this based on your pagination needs
+        const insightsResponse = await fetch(
+          `${configData.SERVER_URL}posts?_embed&categories[]=13&status[]=publish&production_mode[]=${server}&per_page=${page}`,
+        );
+        const posts = await insightsResponse.json();
 
-      const insightsData = await insightsResponse.json();
+        const fetchMedia = async (mediaId) => {
+          const mediaResponse = await fetch(
+            `${configData.SERVER_FROM}media/${mediaId}`,
+          );
+          const mediaData = await mediaResponse.json();
+          return mediaData.source_url;
+        };
 
-      const latestInsights = await Promise.all(
-        insightsData
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
-          .map(async (item) => {
-            const imageUrl = await fetchMedia(item.featured_media);
-            return {
-              ...item,
-              imageUrl: imageUrl,
-              title: item.title.rendered,
-              desc: item.excerpt.rendered,
-            };
-          })
-      );
+        const latestInsights = await Promise.all(
+          posts
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+            .slice(0, 8)
+            .map(async (item) => {
+              const imageUrl = await fetchMedia(
+                item._embedded?.["wp:featuredmedia"]?.[0]?.source_url,
+              );
+              return {
+                ...item,
+                imageUrl: imageUrl,
+                title: item.title.rendered,
+                desc: item.excerpt.rendered,
+              };
+            }),
+        );
 
-      if (latestInsights.length === 0) {
-        setHasMore(false);
-      } else {
         setInsightsData(latestInsights);
-        setHasMore(latestInsights.length > 0);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setLoading(false);
-    }
-  }, [page, domain]);
+    };
 
-  useEffect(() => {
-    fetchContent();
-  }, [fetchContent]);
+    fetchInsights();
+  }, []);
 
   const NextArrow = () => (
     <div
@@ -107,88 +105,111 @@ export default function HomeInsights() {
           infinite: true,
         },
       },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
     ],
   };
 
   return (
-    <div className="lg:h-[80vh]">
-      <div className="z-40 flex h-[650px] flex-col bg-white lg:absolute lg:right-0 lg:-mt-20 lg:w-11/12 lg:flex-row ">
-        <div className="flex justify-between lg:w-2/12">
-          <div className="flex w-full items-center justify-evenly gap-7 lg:flex-col">
-            <h2 className="m-0 py-5 text-2xl font-bold text-custom-red md:p-0 md:text-[80px] lg:-rotate-90">
-              Insights
-            </h2>
-            <div className="hidden gap-4 md:flex">
-              <PrevArrow />
-              <NextArrow />
+    <>
+      <div className="">
+        <div className="z-10 flex h-auto flex-col bg-white lg:mt-10 lg:w-11/12 lg:flex-row">
+          <div className="flex justify-between lg:w-2/12">
+            <div className="flex w-full items-center justify-evenly gap-7 lg:flex-col">
+              <h2 className="m-0 py-5 text-2xl font-bold text-custom-red md:p-0 md:text-[80px] lg:-rotate-90">
+                Insights
+              </h2>
+              {/* Visible only on desktop */}
+              <div className="hidden gap-4 md:flex">
+                <PrevArrow />
+                <NextArrow />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mx-auto w-11/12 lg:w-10/12">
-          {loading ? (
-            <div className="grid justify-between lg:grid-cols-2">
-              {[...Array(2)].map((_, index) => (
-                <div key={index} className="w-full p-2">
-                  <div className="mb-4 h-[300px] animate-pulse bg-gray-200"></div>
-                  <div className="mb-2 h-12 animate-pulse bg-gray-200"></div>
-                  <div className="h-10 animate-pulse bg-gray-200"></div>
-                  <div className="h-10 animate-pulse bg-gray-200"></div>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <InsightSlider ref={sliderRef} {...settings}>
-              {insightsData.map((item, index) => (
-                <div key={index} className="w-full ">
-                  <div className="lg:ms-5 lg:p-4">
-                    <div className="group relative my-auto h-[450px] w-full flex-col border border-gray-200 bg-white shadow transition-colors duration-300 dark:border-gray-700 dark:bg-gray-800 md:hover:bg-custom-red md:hover:text-white lg:flex lg:h-[620px]">
-                      <Image
-                        src={item.imageUrl}
-                        priority={index === 0} // Set high priority for the first slide
-                        loading={index === 0 ? "eager" : "lazy"} // Eager load LCP image
-                        className="h-[200px] w-full object-cover md:h-[280px]"
-                        alt={item.title}
-                        width={600}
-                        height={500}
-                      />
-                      <div className="flex grow flex-col items-start p-5 text-black transition-colors duration-300 md:group-hover:text-white">
-                        <h5
-                          className="mb-3 line-clamp-2 max-h-[4.5rem] min-h-12 overflow-hidden text-lg font-semibold text-custom-blue transition-colors duration-300 md:text-2xl md:group-hover:text-white"
-                          dangerouslySetInnerHTML={{ __html: item.title }}
+          <div className="mx-auto w-11/12 lg:w-10/12">
+            {loading ? (
+              <div className="grid justify-between lg:grid-cols-2">
+                {[...Array(2)].map((_, index) => (
+                  <div key={index} className="w-full p-2">
+                    <div className="mb-4 h-[300px] animate-pulse bg-gray-200"></div>
+                    <div className="mb-2 h-10 animate-pulse bg-gray-200"></div>
+                    <div className="h-14 animate-pulse bg-gray-200"></div>
+                    <div className="h-14 animate-pulse bg-gray-200"></div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <InsightSlider ref={sliderRef} {...settings}>
+                {insightsData.map((item, index) => (
+                  <div key={index} className="w-full">
+                    <div className="lg:ms-5 lg:p-4">
+                      <div className="group relative my-auto h-[450px] w-full flex-col border border-gray-200 bg-white shadow transition-colors duration-300 dark:border-gray-700 dark:bg-gray-800 md:hover:bg-custom-red md:hover:text-white lg:flex lg:h-[620px]">
+                        <Image
+                          src={
+                            item._embedded?.["wp:featuredmedia"]?.[0]
+                              ?.source_url
+                          }
+                          className="h-[200px] w-full object-cover md:h-[280px]"
+                          alt={item.title}
+                          width={600}
+                          height={500}
+                          loading="lazy"
                         />
-                        {item.desc && (
-                          <p
-                            className="mb-5 line-clamp-3 justify-start overflow-hidden text-left text-sm font-normal text-custom-gray transition-colors duration-300 md:text-base md:group-hover:text-white lg:mt-10"
-                            dangerouslySetInnerHTML={{ __html: item.desc }}
+                        <div className="flex grow flex-col items-start p-5 text-black transition-colors duration-300 md:group-hover:text-white">
+                          <h5
+                            className="mb-3 line-clamp-2 max-h-[4.5rem] min-h-12 overflow-hidden text-lg font-semibold text-custom-blue transition-colors duration-300 md:text-2xl md:group-hover:text-white"
+                            dangerouslySetInnerHTML={{ __html: item.title }}
                           />
-                        )}
-                        <Link
-                          href={`/insights/${item.slug}`}
-                          className="absolute bottom-0 left-[35%] m-5 mx-auto block border border-custom-red p-2 text-custom-red transition-colors duration-300 hover:bg-white hover:text-black md:left-5 md:mx-0 md:px-6 md:group-hover:bg-white md:group-hover:text-black"
-                        >
-                          View Article
-                        </Link>
+                          {item.desc && (
+                            <p
+                              className="mb-5 line-clamp-3 justify-start overflow-hidden text-left text-sm font-normal text-custom-gray transition-colors duration-300 md:text-base md:group-hover:text-white lg:mt-10"
+                              dangerouslySetInnerHTML={{ __html: item.desc }}
+                            />
+                          )}
+                          <Link
+                            href={`/insights/${item.slug}`}
+                            className="absolute bottom-0 left-[35%] m-5 mx-auto block border border-custom-red p-2 text-custom-red transition-colors duration-300 hover:bg-white hover:text-black md:left-5 md:mx-0 md:px-6 md:group-hover:bg-white md:group-hover:text-black"
+                          >
+                            View Article
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </InsightSlider>
-          )}
-        </div>
-        <div className="flex items-center justify-center gap-4 pt-8 lg:hidden">
-          <PrevArrow />
-          <NextArrow />
+                ))}
+              </InsightSlider>
+            )}
+            {/* View All Button */}
+            <div className="ms-3 mt-6 flex  justify-center">
+              <Link
+                href="/insights"
+                className="border border-custom-blue px-6 py-2 text-custom-blue md:hover:bg-custom-blue md:hover:text-white"
+              >
+                View all
+              </Link>
+            </div>
+          </div>
+          {/* Mobile view navigation arrows */}
+          <div className="flex items-center justify-center gap-4 pt-8 lg:hidden">
+            <PrevArrow />
+            <NextArrow />
+          </div>
         </div>
       </div>
-      <div className="bottom-0 flex items-end justify-center lg:h-[80vh]">
-        <Link
-          href="/insights"
-          className="border border-custom-blue px-6 py-2 text-custom-blue md:hover:bg-custom-blue md:hover:text-white"
-        >
-          View all
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
